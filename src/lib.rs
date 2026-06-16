dd69e77f4b400824db56eae1398eb414d19ac344
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-06-16 15:11:42 -0700

    Added Error and Result types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 
 #[cfg(not(unix))]
 compile_error!("This library is for unix systems, and the target is not a unix system.");
+
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("IO Error: {0}")]
+    Io(#[from] std::io::Error),
+}
+
+pub type Result<T = (), E = Error> = std::result::Result<T, E>;

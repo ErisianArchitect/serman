311f42d376a669bb6c036201485b727e5453d9a9
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-06-16 15:15:47 -0700

    Added Errno.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 
+use libc::{
+    c_int,
+};
+
 #[cfg(not(unix))]
 compile_error!("This library is for unix systems, and the target is not a unix system.");
 
@@ -10,3 +14,25 @@ pub enum Error {
 }
 
 pub type Result<T = (), E = Error> = std::result::Result<T, E>;
+
+#[must_use]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, thiserror::Error)]
+#[error("Errno({0})")]
+pub struct Errno(c_int);
+
+impl Errno {
+    #[must_use]
+    #[inline(always)]
+    pub fn get() -> Self {
+        unsafe {
+            let ptr = libc::__errno_location();
+            Self(*ptr)
+        }
+    }
+
+    #[must_use]
+    #[inline(always)]
+    pub fn value(self) -> c_int {
+        self.0
+    }
+}

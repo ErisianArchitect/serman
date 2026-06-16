ba18ee36cdf39c9dd3cc562c38186a20a743b3d1
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-06-16 15:08:17 -0700

    Added non-unix compile error.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[cfg(not(unix))]
+compile_error!("This library is for unix systems, and the target is not a unix system.");
